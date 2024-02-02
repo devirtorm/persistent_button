2d6f59e82a336b8f13a8c512223f3ccf8a0e8c16
irtorm <torm@Air-de-irtorm.huawei.net>
2024-02-02 11:20:58 -0700

    correcciones de color

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -2,7 +2,7 @@ function Brand() {
   return (
     <div className="bg-indigo-700 font-black rounded-lg shadow m-4 dark:bg-indigo-700">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="text-sm sm:text-center text-gray-300">
           © 2024{" "}
           <a href="https://linkedin.com/in/devirtorm" className="hover:underline">
             Devirtorm™
